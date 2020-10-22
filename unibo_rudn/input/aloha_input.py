50ac83941938420a4ff0ad50ddfc11271e2a1745
Tsalexey <tsarev.alexey.s@gmail.com>
2020-10-22 21:48:47 +0300

    add input parameters for ALOHA add sample_run and collect_statistics_run for ALOHA

---
+++ unibo_rudn/input/aloha_input.py
@@ -0,0 +1,76 @@
+from unibo_rudn.core.simulation_type import SimulationType
+
+
+class AlohaInput:
+    def __init__(self):
+        self.is_debug = False
+        self.auto_continue = True
+        self.is_debug_cycle_info = False
+        self.is_debug_cycle_error = False
+        self.is_debug_node_info = False
+
+        self.time_limit = True # if False then run simulation until the error between cycle time of all nodes is greater then self.precision
+        self.precision = 0.001
+
+        self.sensing = False
+        self.repeats = 1
+        self.mode = SimulationType.CYCLIC
+
+        # self.planned_success = 25
+        self.simulation_time = 0.0002 # seconds
+
+        self.Nretx = 3  # retransmission attemps, None = unlimited
+        self.NN = 30 # nodes number
+        self.p_a = 1.0 # probability that node has RTS message to transmit
+
+        self.B = 100 * pow(10, 9) # Bandwidth
+        self.eta = 0.5 # Spectral Efficiency
+        self.rb = self.B * self.eta  # Bit rate
+
+        self.sphere_radius = 2 # meter
+        self.c = 3 * pow(10, 8) # meters/second - light speed
+
+        # message length
+        self.Ldata = 0 # do not use for ALOHA
+        self.Ldatapay = 0 # do not use for ALOHA
+        self.Lack = 0 # do not use for ALOHA
+        self.Lrts = 100 # Bytes - ALOHA DATA
+        self.Lcts = 20 # Bytes - ALOHA ACK
+        self.Lbeacon = 00 # do not use for ALOHA
+
+        # times
+        # self.tau_p_max = 0 # self.sphere_radius / self.c # sec - maximal propagation time
+        self.Tdata = (self.Ldata * 8) / self.rb
+        self.Tack = (self.Lack * 8) / self.rb
+        self.Trts = (self.Lrts * 8) / self.rb
+        self.Tcts = (self.Lcts * 8) / self.rb
+        self.Tbeacon = (self.Lbeacon * 8) / self.rb
+        self.Tbo = (self.Lcts * 8) / self.rb
+        self.Tdatacts = self.Tcts + self.Tdata + self.Tack
+        self.Tout = self.Tcts
+        self.Tidle = self.Tcts # o be set according to the Application - it could be also zero
+        self.Twait = self.Tdata + self.Tack
+        self.Tmax = 12 # parameter of the mathematical model
+
+        self.print()
+
+
+    def print(self):
+        print("Input parameters:")
+        print("     mode - ", self.mode)
+        print("     sensing - ", self.sensing)
+        print("     Nretx - ", self.Nretx)
+        print("     NN - ", self.NN)
+        print("     p_a - ", self.p_a)
+        print("     Tdata - ", self.Tdata * pow(10, 9), " ns")
+        print("     Tack - ", self.Tack * pow(10, 9), " ns")
+        print("     Trts - ", self.Trts * pow(10, 9), " ns")
+        print("     Tcts - ", self.Tcts * pow(10, 9), " ns")
+        print("     Tbeacon - ", self.Tbeacon * pow(10, 9), " ns")
+        print("     Tbo - ", self.Tbo * pow(10, 9), " ns")
+        print("     Tdatacts - ", self.Tdatacts * pow(10, 9), " ns")
+        print("     Tout - ", self.Tout * pow(10, 9), " ns")
+        print("     Tidle - ", self.Tidle * pow(10, 9), " ns")
+        print("     Twait - ", self.Twait * pow(10, 9), " ns")
+        print("     Tmax - ", self.Tmax)
+        print("")
---
+++ unibo_rudn/simulation/collect_statistics_aloha_run.py
@@ -0,0 +1,53 @@
+
+import sys
+
+from unibo_rudn.input.aloha_input import AlohaInput
+
+sys.path.append("..")
+
+import csv
+from unibo_rudn.core.statistics_collector import StatisticCollector
+
+
+def main():
+    input = AlohaInput()
+
+    collector = StatisticCollector(input)
+    collector.run()
+
+    output(input, "aloha_statisticts", collector.statistics, collector.statistics_description)
+    output(input, "aloha_statistics_full", collector.detailed_statistics, collector.detailed_statistics_description)
+
+def output(input, file_name, statistics, statistics_description):
+    filename = "../results/" \
+               + file_name \
+               + "_pa[" + str(input.p_a) + "]" \
+               + "_mode[" + input.mode.value + "]" \
+               + "_sensing[" + str(input.sensing) + "]" \
+               + "_nodes[" + str(1) + "-" + str(input.NN) + "]" \
+               + "_radius[" + str(input.sphere_radius) + "]" \
+               + "_retry[" + str(input.Nretx) + "]" \
+               + "_time[" + str(input.simulation_time) + "].dat"
+
+    kwargs = {'newline': ''}
+    mode = 'w'
+
+    with open(filename, mode, **kwargs) as fp:
+        writer = csv.writer(fp, delimiter=';')
+
+        description = []
+        for x in statistics_description.values():
+            description.append(x)
+
+        print(description)
+        writer.writerow(description)
+
+        for keys, values in statistics.items():
+            print(values)
+            writer.writerow(values)
+
+    print()
+
+
+if __name__ == '__main__':
+    main()
