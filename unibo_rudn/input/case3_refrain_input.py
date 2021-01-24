9110f2cdfa458a82ae71717fbb2e261dad3aae0c
Tsalexey <tsarev.alexey.s@gmail.com>
2021-01-24 23:46:51 +0300

    update case3 input

--- unibo_rudn/input/case3_refrain_input.py
+++ unibo_rudn/input/case3_refrain_input.py
@@ -1,3 +1,4 @@
+
 class Case3RefrainInput:
     def __init__(self):
         self.is_debug = False
@@ -8,11 +9,11 @@ class Case3RefrainInput:
 
         self.sensing = False
         self.refrain_from_transmit = True
-        self.repeats = 1
+        self.repeats = 100
 
-        self.simulation_time = 0.00001  # seconds
+        self.simulation_time = 0.000006  # seconds
 
-        self.Nretx = 0  # retransmission attemps, None = unlimited
+        self.Nretx = 3  # retransmission attemps, None = unlimited
         self.NN = 50  # nodes number
         self.p_a = 1.0  # probability that node has RTS message to transmit
 
@@ -24,31 +25,31 @@ class Case3RefrainInput:
         self.c = 3 * pow(10, 8)  # meters/second - light speed
 
         # message length
-        self.Ldata = 3  # Bytes data total
-        self.Ldatapay = 2  # Bytes payload data
-        self.Lack = 3  # Bytes
-        self.Lrts = 3  # Bytes
-        self.Lcts = 3  # Bytes
-        self.Lbeacon = 3  # Bytes
+        self.Ldata = 100 # Bytes data total
+        self.Ldatapay = 80 # Bytes payload data
+        self.Lack = 20 # Bytes
+        self.Lrts = 20 # Bytes
+        self.Lcts = 20 # Bytes
+        self.Lbeacon = 20 # Bytes
 
         # times
         # self.tau_p_max = 0 # self.sphere_radius / self.c # sec - maximal propagation time
 
-        # self.Tslot = (self.Lbeacon * 8) / self.rb / 2.0
-        # self.Tdata = (self.Ldata * 8) / self.rb
-        # self.Tack = (self.Lack * 8) / self.rb
-        # self.Trts = (self.Lrts * 8) / self.rb
-        # self.Tcts = (self.Lcts * 8) / self.rb
-        # self.Tbeacon = (self.Lbeacon * 8) / self.rb
-        # self.Tbo = (self.Lcts * 8) / self.rb
+        self.Tslot = (self.Lbeacon * 8) / self.rb / 2.0
+        self.Tdata = (self.Ldata * 8) / self.rb
+        self.Tack = (self.Lack * 8) / self.rb
+        self.Trts = (self.Lrts * 8) / self.rb
+        self.Tcts = (self.Lcts * 8) / self.rb
+        self.Tbeacon = (self.Lbeacon * 8) / self.rb
+        self.Tbo = (self.Lcts * 8) / self.rb
 
-        self.Tslot = pow(10, -9) / 2.0
-        self.Tdata = pow(10, -9)
-        self.Tack = pow(10, -9)
-        self.Trts = pow(10, -9)
-        self.Tcts = pow(10, -9)
-        self.Tbeacon = pow(10, -9)
-        self.Tbo = pow(10, -9)
+        # self.Tslot = pow(10, -9) / 2.0
+        # self.Tdata = pow(10, -9)
+        # self.Tack = pow(10, -9)
+        # self.Trts = pow(10, -9)
+        # self.Tcts = pow(10, -9)
+        # self.Tbeacon = pow(10, -9)
+        # self.Tbo = pow(10, -9)
 
         self.Tout = self.Tcts
         self.Tidle = self.Trts  # o be set according to the Application - it could be also zero
