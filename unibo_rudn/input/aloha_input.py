ab0529c1103b5258804eb4d94064474cfa643f7c
Tsalexey <tsarev.alexey.s@gmail.com>
2021-01-24 23:23:51 +0300

    update aloha input

--- unibo_rudn/input/aloha_input.py
+++ unibo_rudn/input/aloha_input.py
@@ -8,12 +8,13 @@ class AlohaInput:
         self.is_debug_node_info = False
 
         self.sensing = False
-        self.repeats = 1
+        self.refrain_from_transmit = False
+        self.repeats = 3
 
-        self.simulation_time = 0.0001 # seconds
+        self.simulation_time = 0.000001 # seconds
 
         self.Nretx = 3  # retransmission attemps, None = unlimited
-        self.NN = 10 # nodes number
+        self.NN = 50 # nodes number
         self.p_a = 1.0 # probability that node has RTS message to transmit
 
         self.B = 100 * pow(10, 9) # Bandwidth
@@ -33,18 +34,18 @@ class AlohaInput:
 
         # times
         # self.tau_p_max = 0 # self.sphere_radius / self.c # sec - maximal propagation time
-        self.Tslot = 1 / pow(10, 9)
+        self.Tslot = pow(10, -9)
 
         self.Tdata = 0#(self.Ldata * 8) / self.rb
         self.Tack = 0#(self.Lack * 8) / self.rb
-        self.Trts = 20/pow(10,9)#(self.Lrts * 8) / self.rb
-        self.Tcts = 2/pow(10,9)#(self.Lcts * 8) / self.rb
+        self.Trts = pow(10, -9) #(self.Lrts * 8) / self.rb
+        self.Tcts = pow(10, -9) #(self.Lcts * 8) / self.rb
         self.Tbeacon = 0#(self.Lbeacon * 8) / self.rb
-        self.Tbo = 2/pow(10,9)#(self.Lcts * 8) / self.rb
-        self.Tdatacts = self.Tcts + self.Tdata + self.Tack
-        self.Tout = 2/pow(10,9)#self.Tcts
-        self.Tidle = 2/pow(10,9)#self.Tcts # o be set according to the Application - it could be also zero
-        self.Twait = 0#self.Tdata + self.Tack
+        self.Tbo = pow(10, -9) #(self.Lcts * 8) / self.rb
+        self.Tout = pow(10, -9) #self.Tcts
+        self.Tidle = pow(10, -9) #self.Tcts # o be set according to the Application - it could be also zero
+        self.Trft = 0
+        self.Twait = 00
         self.Tmax = 12 # parameter of the mathematical model
 
         self.print()
@@ -72,7 +73,6 @@ class AlohaInput:
         print("     Tcts - ", self.Tcts * pow(10, 9), " ns")
         print("     Tbeacon - ", self.Tbeacon * pow(10, 9), " ns")
         print("     Tbo - ", self.Tbo * pow(10, 9), " ns")
-        print("     Tdatacts - ", self.Tdatacts * pow(10, 9), " ns")
         print("     Tout - ", self.Tout * pow(10, 9), " ns")
         print("     Tidle - ", self.Tidle * pow(10, 9), " ns")
         print("     Twait - ", self.Twait * pow(10, 9), " ns")
